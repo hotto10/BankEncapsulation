ecb235a553f75038a6ccdea8a4e9af3f388370fb
heather otto <heatherotto32@yahoo.com>
2022-10-28 13:10:24 -0500

    initial commit

--- BankEncapsulation/Program.cs
+++ BankEncapsulation/Program.cs
@@ -4,7 +4,13 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var myAccount = new BankAccount(100);
+            Console.WriteLine($"Your current balance is: {myAccount.GetBalance()}");
+
+            Console.WriteLine("Enter the amount you wish to deposit");
+            double depAmount = int.Parse(Console.ReadLine());
+            myAccount.Deposit(depAmount);
+            Console.WriteLine($"Your new balance is: {myAccount.GetBalance()}");
         }
     }
 }
